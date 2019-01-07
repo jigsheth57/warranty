8dbbee8d51b2f8cbf2862a6aea926d99abff4b97
Jig Sheth <jsheth@pivotal.io>
2019-01-07 13:31:05 -0600

    initial

---
+++ src/test/java/io/pivotal/demo/warranty/WarrantyApplicationTests.java
@@ -0,0 +1,17 @@
+package io.pivotal.demo.warranty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WarrantyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
